752d23bf999c97818d214d5f7e54c17ea7b55294
maria <mariasharif.16@hotmail.com>
2024-06-24 16:40:57 +0100

    part 3 of mvp, adding delete button

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -9,7 +9,9 @@ const enterButton = document.getElementById("enter");
 // Grabbing the enter button and adding an event listener 
 enterButton.addEventListener("click", (event) =>{
 event.preventDefault();
+
 // click is a define event that event listener has
+
 
 // Creating an <li> element and attaching it to the existing <ul>:
 const input = document.getElementById("new-todo");
@@ -17,9 +19,23 @@ const li = document.createElement("li");
 li.innerText = input.value;
 
 
-// Append child
-
+// Add new item to list
 list.appendChild(li)
+
+
+// create delete button 
+const deleteButton = document.createElement("button");
+deleteButton.innerText = "delete";
+
+
+// add delete button to each list element
+li.appendChild(deleteButton);
+
+// functioning delete button
+deleteButton.addEventListener("click", () => {
+    event.preventDefault();
+    li.remove();
+});
 
 });
 
